93f5a41f8e337f5dc408f75f7bf124b412f0f9ca
alimertcan <tekin.mertcan@yahoo.com>
2016-11-27 19:52:42 +0200

    login register
    login register eklendi

--- config/database.php
+++ config/database.php
@@ -56,8 +56,8 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'test'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
@@ -110,8 +110,8 @@ return [
 
         'default' => [
             'host' => env('REDIS_HOST', 'localhost'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
+            'password' => env('REDIS_PASSWORD', ''),
+            'port' => env('REDIS_PORT', 3306),
             'database' => 0,
         ],
 
