aa54d8c086cce294f0c30da3ddf2e42d31e5c455
alimertcan <tekin.mertcan@yahoo.com>
2017-01-12 20:12:39 +0300

    stok

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -28,7 +28,7 @@ class ProductController extends Controller
 	
 	  public function getAddToCart(Request $request,$id){
 	$product=\App\Product::find($id);
-	if($product->stok>=0){
+	if($product->stok>0){
 	$oldCart=Session::has('cart') ? Session::get('cart'):null;
 	$cart = new Cart($oldCart);
 	$cart->add($product, $product->id);
